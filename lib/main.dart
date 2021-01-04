7de94b47420c2672c404c5d900659a054aebb462
Sébastien <sebi.marquis@gmail.com>
2021-01-04 13:47:19 +0100

    merge master into ios branch to run on ios

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:date_field/date_field.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:date_field/date_field.dart';
 import 'package:intl/intl.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
